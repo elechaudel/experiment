fe795bafd4403828b9f2ca972816bd90a9245675
Edgar Lechaudel <elechaudel@gmail.com>
2018-01-27 17:59:06 +0100

    - new layout structure - shapes animation

---
+++ geometrical-design/scroll.js
@@ -0,0 +1,7 @@
+// $(".js-anchor").click(function(){
+//   $("body").scrollTo($(this).data("target"), 300, {offset:-70});
+// });
+//
+// $(".navbar-logo").click(function(){
+//   $("body").scrollTo(0, 300);
+// });
